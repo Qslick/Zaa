81e78d07a95854620f60e9061b354bd3786d4f57
Jason Bradley <jason.bradley@riskonnect.com>
2017-09-26 23:21:30 -0400

    Updated gitignore

--- platforms/android/src/main/assets/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { Order } from "./shared/order/order";
-
-@Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html",
-})
-
-export class AppComponent { }
